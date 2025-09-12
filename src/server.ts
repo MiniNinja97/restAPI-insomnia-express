a239330c703c5f3ff090a93c1178aaa9f6d8b1c9
Emma Malinsdotter <emma.malinsdotter@outlook.com>
2025-09-12 10:32:25 +0200

    har börjat med en html

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,28 @@
 import express from "express";
-import type { RequestHandler } from "express";
+
 import moviesRouter from "./routes/routing.js"; 
-import cors from 'cors';
+import cors from "cors";
 import authenticate from "./middleware/auth.js";
 import hiddenMovieRouter from "./routes/hiddenMovie.js";
 import logger from "./middleware/logger.js";
 
-
-
 const app = express();
 const port = 12345;
-
 
 app.use(cors());
 app.use(express.json());
 app.use(logger);
-app.use('/', authenticate);
-app.use('/secret', hiddenMovieRouter);
+
+
+app.use(express.static("public"));
+
+
+app.use("/secret", authenticate, hiddenMovieRouter);
 app.use("/movies", moviesRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
 
 
